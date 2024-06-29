8433d939bba1019313b34a482d423e6127014155
JonatanRasmussen <115923496+JonatanRasmussen@users.noreply.github.com>
2024-06-29 03:43:47 +0200

    Renamed wowhead cache folder

--- global_configs.py
+++ global_configs.py
@@ -7,7 +7,7 @@ class GlobalConfigs:
 
     # Folders and file names
     EXCEL_SHEET_NAME = "mythicplus_tww.xlsx"
-    CACHED_WOWHEAD_PAGES = "wowhead"
+    CACHED_WOWHEAD_PAGES = "wowhead_cache"
     CACHED_WOWHEAD_SPELL_ID_PAGES = os.path.join(CACHED_WOWHEAD_PAGES, "wowhead_spell_ids")
     CACHED_WOWHEAD_ZONE_ID_PAGES = os.path.join(CACHED_WOWHEAD_PAGES, "wowhead_zone_ids")
 
